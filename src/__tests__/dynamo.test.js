5cd87cb31dc080ed7e4933b5ead4d3a5702958d7
unknown <code.triplett@gmail.com>
2019-09-10 00:47:27 -0500

    refactored dynamo

--- src/__tests__/dynamo.test.js
+++ src/__tests__/dynamo.test.js
@@ -5,12 +5,32 @@ import { dynamo } from '../dynamo';
 jest.mock('../evaluate', () => ({ evaluate: jest.fn() }));
 jest.mock('../render', () => ({ render: jest.fn() }));
 
+function createContainer (...ids) {
+	const childNodes = ids.map(id => ({ getAttribute: () => id }));
+
+	childNodes.reduce((previousSibling, childNode) => {
+		childNode.previousSibling = previousSibling;
+		return childNode;
+	}, null);
+	
+	return { childNodes };
+}
+
 function check (resolve, variations, ...inputs) {
 	const alternative = resolve === evaluate ? render : evaluate;
+	const callback = inputs.pop();
 	const state = {};
 
 	for (const name in variations) {
-		it(name, () => {
+		const mode = it;
+
+		if (name.startsWith('!')) {
+			mode = it.skip;
+		} else if (name.endsWith('!')) {
+			mode = it.only;
+		}
+
+		mode(name.replace(/^!+|!+$/, ''), () => {
 			const [expression, ...calls] = variations[name];
 			const actual = dynamo(expression, state, ...inputs);
 			const expected = calls.pop();
@@ -19,7 +39,7 @@ function check (resolve, variations, ...inputs) {
 				item,
 				...parameters
 			]) => {
-				parameters.push(...inputs.slice(parameters.length));
+				parameters = callback(...inputs, ...parameters);
 				return [item, state, ...parameters];
 			}));
 
@@ -31,8 +51,8 @@ function check (resolve, variations, ...inputs) {
 
 describe('dynamo', () => {
 	beforeEach(() => {
-		evaluate.mockClear().mockImplementation(([key]) => {
-			return typeof key === 'string' ? `(${key})` : key;
+		evaluate.mockClear().mockImplementation(([key, value = key]) => {
+			return typeof value === 'string' ? `(${value})` : value;
 		});
 
 		render.mockClear().mockImplementation(item => {
@@ -40,161 +60,183 @@ describe('dynamo', () => {
 		});
 	});
 
-	describe('evaluate', () => {
+	describe.only('evaluate', () => {
 		check(evaluate, {
-			'static': [['a'], ['a']],
-			'dynamic': [[['a']], [['a']], ['(a)']],
-			'match': [[[true]], [[true]], []],
-			'mismatch': [[[false]], [[false]], false],
-			'static static': [['a', 'b'], ['a', 'b']],
-			'static dynamic': [['a', ['b']], [['b']], ['a', '(b)']],
-			'static match': [['a', [true]], [[true]], ['a']],
-			'static mismatch': [['a', [false]], [[false]], false],
-			'dynamic static': [[['a'], 'b'], [['a']], ['(a)', 'b']],
+			'static': [['a'], 'a'],
+			'dynamic': [[['a']], [['a']], '(a)'],
+			'match': [[['a', true]], [['a', true]], true],
+			'mismatch': [[['a', false]], [['a', false]], false],
+			'static static': [['a', 'b'], 'ab'],
+			'static dynamic': [['a', ['b']], [['b']], 'a(b)'],
+			'static match': [['a', ['b', true]], [['b', true]], 'a'],
+			'static mismatch': [['a', ['b', false]], [['b', false]], false],
+			'dynamic static': [[['a'], 'b'], [['a']], '(a)b'],
 			'dynamic dynamic': [
 				[['a'], ['c']],
-				[['c']],
-				[['a']],
-				['(a)', '(c)']
+				[['c']], [['a']],
+				'(a)(c)'
 			],
-			'dynamic match': [[['a'], [true]], [[true]], [['a']], ['(a)']],
-			'dynamic mismatch': [[['a'], [false]], [[false]], false],
-			'match static': [[[true], 'b'], [[true]], ['b']],
-			'match dynamic': [[[true], ['b']], [['b']], [[true]], ['(b)']],
-			'match match': [[[true], [true]], [[true]], [[true]], []],
-			'match mismatch': [[[true], [false]], [[false]], false],
-			'mismatch static': [[[false], 'b'], [[false]], ['']],
-			'mismatch dynamic': [
-				[[false], ['b']],
-				[['b']],
-				[[false]],
-				['']
+			'dynamic match': [
+				[['a'], ['b', true]],
+				[['b', true]], [['a']],
+				'(a)'
 			],
-			'mismatch match': [
-				[[false], [true]],
-				[[true]],
-				[[false]],
+			'dynamic mismatch': [[['a'], ['b', false]], [['b', false]], false],
+			'match static': [[['a', true], 'b'], [['a', true]], 'b'],
+			'match dynamic': [
+				[['a', true], ['b']],
+				[['b']], [['a', true]],
+				'(b)'
+			],
+			'match match': [
+				[['a', true], ['b', true]],
+				[['b', true]], [['a', true]],
+				true
+			],
+			'match mismatch': [
+				[['a', true], ['b', false]],
+				[['b', false]],
 				false
 			],
-			'mismatch mismatch': [[[false], [false]], [[false]], false]
-		}, {});
+			'mismatch static': [[['a', false], 'b'], [['a', false]], ''],
+			'mismatch dynamic': [
+				[['a', false], ['b']],
+				[['b']], [['a', false]],
+				''
+			],
+			'mismatch match': [
+				[['a', false], ['b', true]],
+				[['b', true]], [['a', false]],
+				false
+			],
+			'mismatch mismatch': [
+				[['a', false], ['b', false]],
+				[['b', false]],
+				false
+			]
+		}, () => [undefined]);
+	});
+
+	describe('hydrate', () => {
+		check(evaluate, {
+			'static': [['a'], 'a']
+		}, 'a', (value, fragment) => [fragment, undefined]);
+		
+		check(evaluate, {
+			'dynamic': [[['a']], [['a'], '(a)'], '(a)']
+		}, '(a)', (value, fragment) => [fragment, undefined]);
+		
+		check(evaluate, {
+			'match': [[[true]], [[true], ''], true]
+		}, '', (value, fragment) => [fragment, undefined]);
+
+		check(evaluate, {
+			'static static': [['a', 'b'], 'ab']
+		}, 'ab', (value, fragment) => [fragment, undefined]);
+		
+		check(evaluate, {
+			'static dynamic': [['a', ['b']], [['b'], '(b)'], 'a(b)']
+		}, 'a(b)', (value, fragment) => [fragment]);
+		
+		check(evaluate, {
+			'static match': [['a', [true]], [[true], 'a'], 'a']
+		}, 'a', (value, fragment) => [fragment, undefined]);
+		
+		check(evaluate, {
+			'dynamic static': [[['a'], 'b'], [['a'], '(a)'], '(a)b']
+		}, '(a)b', (value, fragment) => [fragment, undefined]);
+
+		check(evaluate, {
+			'dynamic dynamic': [
+				[['a'], ['c']],
+				[['c'], '(a)(c)'],
+				[['a'], '(a)(c)'],
+				'(a)(c)'
+			]
+		}, '(a)(c)', (value, fragment) => [fragment, undefined]);
+
+		check(evaluate, {
+			'dynamic match': [
+				[['a'], [true]],
+				[[true], '(a)'],
+				[['a'], '(a)'],
+				'(a)'
+			]
+		}, '(a)', (value, fragment) => [fragment, undefined]);
 	});
 
 	describe('generate', () => {
 		check(render, {
-			'expression': [[['a']], [['a'], 0], ['[a]']],
-			'element': [[{ '': 'a' }], [{ '': 'a' }, 0], ['<a>']],
+			'expression': [[['a']], [['a'], 0], '[a]'],
+			'element': [[{ '': 'a' }], [{ '': 'a' }, 0], '<a>'],
 			'expression expression': [
 				[['a'], ['b']],
 				[['b'], 1],
 				[['a'], 0],
-				['[a]', '[b]']
+				'[a][b]'
 			],
 			'expression element': [
 				[['a'], { '': 'b' }],
 				[{ '': 'b' }, 1],
 				[['a'], 0],
-				['[a]', '<b>']
+				'[a]<b>'
 			],
 			'element expression': [
 				[{ '': 'a' }, ['b']],
 				[['b'], 1],
 				[{ '': 'a' }, 0],
-				['<a>', '[b]']
+				'<a>[b]'
 			],
 			'element element': [
 				[{ '': 'a' }, { '': 'b' }],
 				[{ '': 'b' }, 1],
 				[{ '': 'a' }, 0],
-				['<a>', '<b>']
+				'<a><b>'
 			]
-		}, 0, {});
+		}, {}, (backup, index) => [index, backup, undefined]);
 	});
 
 	describe('update', () => {
-		const nodes = [
-			{ getAttribute: () => null },
-			{ getAttribute: () => '2' },
-			{ getAttribute: () => null },
-			{ getAttribute: () => '3-0' },
-			{ getAttribute: () => '3-1' },
-			{ getAttribute: () => null }
-		];
-
 		const update = () => {};
-
-		nodes.forEach((node, i) => Object.assign(node, {
-			nextSibling: nodes[i + 1] || null
-		}));
+		const container = createContainer(null, null);
 
 		check(render, {
 			'expression': [
 				[['a']],
-				[['a'], 0, nodes.slice(0, 1)],
-				['[a]']
+				[['a'], 0, container.childNodes.slice(1, 2)],
+				'[a]'
 			],
 			'element': [
 				[{ '': 'a' }],
-				[{ '': 'a' }, 0, nodes.slice(0, 1)],
-				['<a>']
+				[{ '': 'a' }, 0, container.childNodes.slice(1, 2)],
+				'<a>'
 			],
 			'expression expression': [
 				[['a'], ['b']],
-				[['b'], 1, []],
-				[['a'], 0, nodes.slice(0, 1)],
-				['[a]', '[b]']
+				[['b'], 1, container.childNodes.slice(1, 2)],
+				[['a'], 0, container.childNodes.slice(0, 1)],
+				'[a][b]'
 			],
 			'expression element': [
 				[['a'], { '': 'b' }],
-				[{ '': 'b' }, 1, []],
-				[['a'], 0, nodes.slice(0, 1)],
-				['[a]', '<b>']
+				[{ '': 'b' }, 1, container.childNodes.slice(1, 2)],
+				[['a'], 0, container.childNodes.slice(0, 1)],
+				'[a]<b>'
 			],
 			'element expression': [
 				[{ '': 'a' }, ['b']],
-				[['b'], 1, []],
-				[{ '': 'a' }, 0, nodes.slice(0, 1)],
-				['<a>', '[b]']
+				[['b'], 1, container.childNodes.slice(1, 2)],
+				[{ '': 'a' }, 0, container.childNodes.slice(0, 1)],
+				'<a>[b]'
 			],
 			'element element': [
 				[{ '': 'a' }, { '': 'b' }],
-				[{ '': 'b' }, 1, []],
-				[{ '': 'a' }, 0, nodes.slice(0, 1)],
-				['<a>', '<b>']
+				[{ '': 'b' }, 1, container.childNodes.slice(1, 2)],
+				[{ '': 'a' }, 0, container.childNodes.slice(0, 1)],
+				'<a><b>'
 			]
-		}, 0, nodes[0], update);
-
-		check(render, {
-			'skip missing': [
-				[['a']],
-				[['a'], 1, []],
-				['[a]']
-			]
-		}, 1, nodes[1], update);
-
-		check(render, {
-			'update after skip': [
-				[['a']],
-				[['a'], 2, nodes.slice(1, 2)],
-				['[a]']
-			]
-		}, 2, nodes[1], update);
-		
-		check(render, {
-			'update loop': [
-				[['a']],
-				[['a'], 3, nodes.slice(3, 5)],
-				['[a]']
-			]
-		}, 3, nodes[3], update);
-		
-		check(render, {
-			'skip at end': [
-				[['a'], ['b']],
-				[['b'], 6, null],
-				[['a'], 5, nodes.slice(5, 6)],
-				['[a]', '[b]']
-			]
-		}, 5, nodes[5], update);
+		}, container, update, (container, update, index, nodes) => {
+			return [index, nodes, update];
+		});
 	});
 });
--- src/dynamo.js
+++ src/dynamo.js
@@ -1,66 +1,94 @@
 import { evaluate } from './evaluate';
 import { render } from './render';
 
-export function dynamo ([item, ...items], state, ...parameters) {
-	const dynamic = typeof item === 'object';
-	const advancement = [...parameters];
-	let [index, node] = parameters;
-	let resolve = evaluate;
-	let skip = false;
+export function dynamo (items, state, element, update) {
+	const hydrate = typeof element === 'string' || undefined;
 
-	if (typeof index === 'number') {
-		if (node && typeof node.getAttribute === 'function') {
-			const { parentElement } = node;
-			const regex = new RegExp(`^${index}(-|$)`);
-			const nodes = [];
+	if (update) {
+		const children = element.childNodes;
+		element = children[children.length - 1];
+	}
 
-			while (index !== null && node) {
-				index = node.getAttribute('data--');
+	const values = items.reduceRight((values, item, i) => {
+		if (!values) {
+			return values;
+		}
+		
+		let [value = true] = values;
+		
+		if (typeof element === 'object') {
+			const elements = update ? [] : element;
+			
+			if (update) {
+				const regex = new RegExp(`^${i}(-|$)`);
+				let id;
 
-				if (index && !regex.test(index) || !index && nodes.length) {
-					break;
+				while (id !== null && element) {
+					id = element.getAttribute('data--');
+
+					if (id && !regex.test(id) || !id && elements.length) {
+						break;
+					}
+
+					elements.push(element);
+					element = element.previousSibling;
+				}
+			}
+
+			value = render(item, state, i, elements, update);
+		} else if (typeof item !== 'string') {
+			const compare = item.length > 1;
+
+			if (!compare) {
+				if (hydrate) {
+					const string = items.slice(0, i).reverse().find(item => {
+						return !Array.isArray(item) && element.includes(item);
+					});
+
+					if (string) {
+						const { length } = string;
+						const index = element.lastIndexOf(string) + length;
+
+						value = element.slice(index);
+						element = element.slice(0, index);
+					}
+				} else {
+					value = true;
+				}
+			}
+
+			value = value && evaluate(item, state, hydrate && value);
+			
+			if (compare) {
+				if (value === false) {
+					if (!values.length) {
+						return value;
+					}
+					
+					values[0] = '';
 				}
 
-				nodes.push(node);
-				node = node.nextSibling;
+				return values;
 			}
+		} else {
+			value = item;
 
-			advancement[1] = node;
-			parameters[1] = nodes;
+			if (element) {
+				if (element.endsWith(item)) {
+					element = element.slice(0, -item.length);
+				} else {
+					value = '';
+				}
+			}
 		}
 
-		advancement[0]++;
-		resolve = render;
-	} else if (dynamic) {
-		skip = item.length > 1;
-	} else if (Array.isArray(index)) {
-		index.push(item);
+		values.unshift(value);
+		return values;
+	}, []);
+
+	if (!Array.isArray(values)) {
+		return values;
 	}
 
-	if (items.length) {
-		items = dynamo(items, state, ...advancement);
-	}
-
-	if (items) {
-		if (dynamic) {
-			if (skip) {
-				update = items[0];
-				skip = !update;
-			}
-
-			item = skip || resolve(item, state, ...parameters);
-		}
-
-		if (typeof item !== 'boolean') {
-			items.unshift(item);
-		} else if (!item) {
-			if (!items.length) {
-				return false;
-			}
-
-			items[0] = '';
-		}
-	}
-
-	return items;
+	return !values.length || values.join('');
 }
