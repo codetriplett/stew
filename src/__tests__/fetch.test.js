634b0ec19f7a5d462eb0cc43fb7f3549d731c2b6
unknown <code.triplett@gmail.com>
2019-09-17 23:10:24 -0500

    added index fetching and listeners

--- src/__tests__/fetch.test.js
+++ src/__tests__/fetch.test.js
@@ -31,6 +31,37 @@ describe('fetch', () => {
 			expect(actual).toBe('xyz');
 		});
 
+		it('parent context', () => {
+			state[''] = { string: 'cba' };
+			const actual = fetch(['.string'], state);
+
+			expect(backup).toEqual({});
+			expect(actual).toBe('cba');
+		});
+
+		it('current index', () => {
+			state['.'].indices = [1, 2];
+			const actual = fetch(['.'], state);
+
+			expect(backup).toEqual({});
+			expect(actual).toBe(1);
+		});
+
+		it('parent index', () => {
+			state['.'].indices = [1, 2];
+			const actual = fetch(['..'], state);
+
+			expect(backup).toEqual({});
+			expect(actual).toBe(2);
+		});
+
+		it('max index', () => {
+			const actual = fetch(['array.'], state);
+
+			expect(backup).toEqual({});
+			expect(actual).toBe(1);
+		});
+
 		it('stores backup', () => {
 			const actual = fetch(['string.'], state);
 
@@ -52,13 +83,6 @@ describe('fetch', () => {
 			expect(actual).toBe(false);
 		});
 
-		it('max index', () => {
-			const actual = fetch(['number', 'array.'], state);
-
-			expect(backup).toEqual({});
-			expect(actual).toBe(true);
-		});
-
 		it('match with variable', () => {
 			const actual = fetch(['string', 'string.'], state);
 
@@ -71,27 +95,27 @@ describe('fetch', () => {
 		let state;
 
 		beforeEach(() => {
-			state = {};
+			state = { '.': {} };
 		});
 
 		it('string', () => {
 			const actual = fetch(['string'], state, 'abc');
 
-			expect(state).toEqual({ string: 'abc' });
+			expect(state).toEqual({ '.': expect.anything(), string: 'abc' });
 			expect(actual).toBe('abc');
 		});
 	
 		it('number', () => {
 			const actual = fetch(['number'], state, '123');
 
-			expect(state).toEqual({ number: 123 });
+			expect(state).toEqual({ '.': expect.anything(), number: 123 });
 			expect(actual).toBe(123);
 		});
 	
 		it('empty', () => {
 			const actual = fetch(['empty'], state, '');
 
-			expect(state).toEqual({});
+			expect(state).toEqual({ '.': expect.anything() });
 			expect(actual).toBe('');
 		});
 
@@ -99,31 +123,61 @@ describe('fetch', () => {
 			const actual = fetch(['object.string'], state, 'xyz');
 
 			expect(state).toEqual({
-				object: { '': expect.anything(), string: 'xyz' }
+				'.': expect.anything(),
+				object: {
+					'': expect.anything(),
+					'.': expect.anything(),
+					string: 'xyz'
+				}
 			});
 
 			expect(actual).toBe('xyz');
+		});
+
+		it('parent context', () => {
+			const object = {};
+			state[''] = object;
+			const actual = fetch(['.string'], state, 'cba');
+
+			expect(object).toEqual({ string: 'cba' });
+			expect(actual).toBe('cba');
+		});
+
+		it('current index', () => {
+			state['.'].indices = [1, 2];
+			const actual = fetch(['.'], state, '1');
+
+			expect(state).toEqual({ '.': expect.anything() });
+			expect(actual).toBe(1);
+		});
+
+		it('parent index', () => {
+			state['.'].indices = [1, 2];
+			const actual = fetch(['..'], state, '2');
+
+			expect(state).toEqual({ '.': expect.anything() });
+			expect(actual).toBe(2);
+		});
+
+		it('ignores max index', () => {
+			const actual = fetch(['array.'], state, '1');
+
+			expect(state).toEqual({ '.': expect.anything() });
+			expect(actual).toBe('1');
 		});
 
 		it('uses backup', () => {
 			state.string = 'abc';
 			const actual = fetch(['string.'], state, 'xyz');
 
-			expect(state).toEqual({ string: 'abc' });
+			expect(state).toEqual({ '.': expect.anything(), string: 'abc' });
 			expect(actual).toBe('xyz');
-		});
-
-		it('ignores max index', () => {
-			const actual = fetch(['number', 'array.'], state, '1');
-
-			expect(state).toEqual({});
-			expect(actual).toBe(true);
 		});
 	
 		it('condition', () => {
 			const actual = fetch(['string', 'abc'], state, '');
 
-			expect(state).toEqual({ string: 'abc' });
+			expect(state).toEqual({ '.': expect.anything(), string: 'abc' });
 			expect(actual).toBe(true);
 		});
 
@@ -131,8 +185,74 @@ describe('fetch', () => {
 			state.string = 'abc';
 			const actual = fetch(['string', 'string.'], state, 'abc');
 
-			expect(state).toEqual({ string: 'abc' });
+			expect(state).toEqual({ '.': expect.anything(), string: 'abc' });
 			expect(actual).toBe(true);
+		});
+	});
+
+	describe('activate', () => {
+		const update = jest.fn();
+		let state;
+
+		beforeEach(() => {
+			update.mockClear();
+			state = { flag: false, index: 0 };
+		});
+
+		it('creates toggle', () => {
+			const actual = fetch(['flag'], state, 'onclick', update);
+
+			expect(state).toMatchObject({ flag: false });
+			expect(actual).toEqual(expect.any(Function));
+
+			actual();
+
+			expect(state).toMatchObject({ flag: true });
+			expect(update).toHaveBeenCalled();
+		});
+
+		it('creates incrementer', () => {
+			const actual = fetch(['index', 2], state, 'onclick', update);
+
+			expect(state).toMatchObject({ index: 0 });
+			expect(actual).toEqual(expect.any(Function));
+
+			actual();
+
+			expect(state).toMatchObject({ index: 1 });
+			expect(update).toHaveBeenCalled();
+
+			actual();
+
+			expect(state).toMatchObject({ index: 2 });
+			expect(update).toHaveBeenCalled();
+
+			actual();
+
+			expect(state).toMatchObject({ index: 0 });
+			expect(update).toHaveBeenCalled();
+		});
+
+		it('creates decrementer', () => {
+			const actual = fetch(['index.', 2], state, 'onclick', update);
+
+			expect(state).toMatchObject({ index: 0 });
+			expect(actual).toEqual(expect.any(Function));
+
+			actual();
+
+			expect(state).toMatchObject({ index: 2 });
+			expect(update).toHaveBeenCalled();
+
+			actual();
+
+			expect(state).toMatchObject({ index: 1 });
+			expect(update).toHaveBeenCalled();
+
+			actual();
+
+			expect(state).toMatchObject({ index: 0 });
+			expect(update).toHaveBeenCalled();
 		});
 	});
 });
--- src/fetch.js
+++ src/fetch.js
@@ -1,53 +1,81 @@
-export function fetch (item, state, value) {
+export function fetch (item, state, value, update) {
 	let [key, comparison] = item;
 	const compare = item.length > 1;
 	const measure = key.endsWith('.');
 	const hydrate = value !== undefined;
-	const keys = key.replace(/\.+$/, '').split('.');
+	const keys = key.replace(/\.$/, '').split('.');
 
 	key = keys.pop();
 
-	state = keys.reduce((state, key) => {
-		if (!state.hasOwnProperty(key)) {
-			const { '.': settings } = state;
-			const object = { '': state };
+	if (key) {
+		state = keys.reduce((state, key) => {
+			if (!state.hasOwnProperty(key)) {
+				const object = { '': state };
+				let { '.': { backup, ...settings } } = state;
 
-			if (settings) {
-				let { backup } = settings;
-				
-				backup = backup[key] = { '': backup };
+				if (backup) {
+					backup = backup[key] = { '': backup };
+				}
+
 				object['.'] = { ...settings, backup };
+				state[key] = object;
 			}
-
-			state[key] = object;
-		}
+			
+			return state[key];
+		}, state);
 		
-		return state[key];
-	}, state);
+		if (!hydrate) {
+			value = state[key];
 
-	if (!hydrate) {
-		value = state[key];
+			if (measure) {
+				const { backup } = state['.'];
 
-		if (measure) {
-			const { backup } = state['.'];
-
-			if (Array.isArray(value)) {
-				value = value.length - 1;
-			} else if (backup && !backup.hasOwnProperty(key)) {
-				fetch([key], backup, value);
+				if (Array.isArray(value)) {
+					value = value.length - 1;
+				} else if (backup && !backup.hasOwnProperty(key)) {
+					fetch([key], backup, value);
+				}
 			}
+		} else if (update && !compare) {
+			return () => {
+				const { [key]: previous } = state;
+
+				switch (value) {
+					case 'onclick': {
+						state[key] = !previous;
+					}
+				}
+
+				update();
+			};
 		}
+	} else {
+		value = state['.'].indices[keys.length];
 	}
 
 	if (compare) {
 		if (typeof comparison === 'string' && comparison.endsWith('.')) {
-			comparison = hydrate ? value : fetch([comparison], state);
-		} else if (hydrate) {
+			comparison = fetch([comparison], state);
+		} else if (hydrate && !update) {
 			value = state[key] = comparison;
 		}
 
+		if (update) {
+			return () => {
+				const { [key]: previous } = state;
+
+				if (measure) {
+					state[key] = previous ? previous - 1 : comparison;
+				} else {
+					state[key] = previous < comparison ? previous + 1 : 0;
+				}
+
+				update();
+			};
+		}
+
 		value = value === comparison;
-	} else if (hydrate && value && !state.hasOwnProperty(key)) {
+	} else if (value && !measure && !state.hasOwnProperty(key)) {
 		value = state[key] = !isNaN(value) ? Number(value) : value;
 	} else if (typeof value === 'boolean') {
 		value = '';
