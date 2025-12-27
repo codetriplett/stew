1f4a906b8dc5a576f18e4b0d7857f2c99d621266
jeffreytriplett <jeffreytriplett@users.noreply.github.com>
2025-12-26 23:14:01 -0600

    program rewrites

--- preview/cube.mjs
+++ preview/cube.mjs
@@ -240,7 +240,6 @@ export function cube () {
                 mat3 uMatrix ${matrix}
                 vec3 uOffset ${offset}
                 ${gl => gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0)}
-                //
             `)}
             vec3 absPos = abs(vPos);
             float xEdge = max(absPos.y, absPos.z);
--- src/program.js
+++ src/program.js
@@ -1,15 +1,10 @@
-import { processMemo } from './impulse';
+import { execute, processMemo } from './impulse';
 import { animations, schedule } from './state';
 
-export const sequenceMap = new WeakMap();
-export const rootMap = new WeakMap();
-export const varyingMap = new WeakMap();
-export const programMap = new WeakMap();
-export const setterMap = new WeakMap();
-export const siblingMap = new Map();
-export const vertexStack = [[new WeakMap()]];
-export const fragmentStack = [[new WeakMap()]];
-export const sceneChain = [];
+const rootMap = new WeakMap();
+const setterMap = new WeakMap();
+const vertexRoot = [new WeakMap()];
+const fragmentRoot = [new WeakMap()];
 
 const setterNames = {
 	uint: 'uniform1u',
@@ -29,12 +24,12 @@ const setterNames = {
 	mat4: 'uniformMatrix4fv',
 };
 
-function getStored (map, key, callback) {
+function getStored (map, key, callback, ...params) {
 	if (map.has(key)) {
 		return map.get(key);
 	}
 
-	const value = callback();
+	const value = callback(key, ...params);
 	map.set(key, value);
 	return value;
 }
@@ -141,14 +136,14 @@ function createOtherSetter (gl, subname, name) {
 	}
 }
 
-function createCode (stack, varyings) {
-	const isFragment = stack === fragmentStack;
+function createCode (stack, varyings, type) {
+	const isFragment = type === 'FRAGMENT_SHADER';
 	const precisionCode = new Set();
 	const allCode = [];
 	const allVars = [];
 	const headerCode = isFragment ? ['out vec4 gl2_FragColor;'] : [];
 
-	for (const [, info] of stack.slice(0, -1)) {
+	for (const [info] of stack.slice(1)) {
 		const [, [...code], ...vars] = info;
 
 		while (code[0]?.startsWith?.('precision ')) {
@@ -196,10 +191,9 @@ function createCode (stack, varyings) {
 	].join('\n');
 }
 
-export function createShader (gl, stack, varyings) {
-	return getStored(stack[0][0], gl, () => {
-		const code = createCode(stack, varyings);
-		const type = stack === fragmentStack ? 'FRAGMENT_SHADER' : 'VERTEX_SHADER';
+export function createShader (gl, stack, varyings, type) {
+	return getStored(stack[0], gl, () => {
+		const code = createCode(stack, varyings, type);
 		const shader = gl.createShader(gl[type]);
 		gl.shaderSource(shader, code);
 		gl.compileShader(shader);
@@ -210,6 +204,89 @@ export function createShader (gl, stack, varyings) {
 
 		return shader;
 	});
+}
+
+function createSetter (gl, program, variables, values) {
+	const scopedSetterMap = getStored(setterMap, gl, () => new WeakMap());
+
+	const setters = variables.map(variable => {
+		return getStored(scopedSetterMap, variable, () => {
+			if (variable.length < 3) {
+				return createOtherSetter(gl, ...variable);
+			}
+
+			const type = variable[2];
+			const subtype = variable[3];
+			const create = !subtype || type === 'sampler2D' ? createUniformSetter : createAttributeSetter;
+			return create(gl, program, ...variable);
+		});
+	});
+
+	return (gl, duration) => {
+		for (const [i, setter] of setters.entries()) {
+			let value = values[i];
+
+			if (typeof value === 'function') {
+				value = value(duration);
+			}
+
+			setter(value);
+		}
+	};
+}
+
+function getGl (canvas) {
+	return canvas.getContext('webgl2', {
+		premultipliedAlpha: /^(transparent)?$/.test(canvas.style.background),
+		antialias: !/^(crisp-edges|pixelated)$/.test(canvas.style.imageRendering),
+	});
+}
+
+function createProgram (canvas, vertexStack, fragmentStack, siblingMap, ...callbacks) {
+	const gl = getStored(rootMap, canvas, getGl);
+
+	const program = getStored(vertexStack[0], fragmentStack, () => {
+		const varyings = getStored(fragmentStack[0], vertexStack, () => []);
+		const vertexShader = createShader(gl, vertexStack, varyings, 'VERTEX_SHADER');
+		const fragmentShader = createShader(gl, fragmentStack, varyings, 'FRAGMENT_SHADER');
+		const program = gl.createProgram();
+		gl.attachShader(program, vertexShader);
+		gl.attachShader(program, fragmentShader);
+		gl.linkProgram(program);
+
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+			console.error(gl.getProgramInfoLog(program));
+		}
+
+		return program;
+	});
+
+	const isNew = siblingMap.has(program);
+	const entry = getStored(siblingMap, program, () => [program]);
+	const composite = [...vertexStack.slice(1), ...fragmentStack.slice(1)];
+	const allVariables = [];
+	const allValues = [];
+
+	for (const [info, set, values] of composite) {
+		if (set.has(entry)) {
+			continue;
+		}
+
+		set.add(entry);
+		allVariables.push(...info.slice(2));
+		allValues.push(...values);
+	}
+
+	if (allVariables.length) {
+		const setter = createSetter(gl, program, allVariables, allValues);
+		entry.push(setter);
+	}
+
+	entry.push(...callbacks);
+
+	if (isNew) {
+		return entry;
+	}
 }
 
 export function parse (strings) {
@@ -249,129 +326,59 @@ export function parse (strings) {
 			return line;
 		});
 		
+		// const stack = i ? fragmentStack : vertexStack;
 		const minIndentation = Math.min(...indentation);
 		info[1] = codeLines.map(line => line.slice(minIndentation));
+		// return createSetup(stack, info);
 	}
 
 	return sequence;
 }
 
-function createSetter (gl, program, variables, values) {
-	const scopedSetterMap = getStored(setterMap, gl, () => new WeakMap());
+export function extract (canvas, stack, info, values, vertexStack, siblingMap) {
+	const [labels,, ...variables] = info;
+	const resolvers = values.splice(0, labels.length);
+	stack = getStored(stack[0], info, () => [new WeakMap(), [info], ...stack.slice(1)]);
+	const chain = [];
+	let callbacks = [];
+	stack[1].splice(1, 2, new WeakSet(), values.splice(0, variables.length));
 
-	const setters = variables.map(variable => {
-		return getStored(scopedSetterMap, variable, () => {
-			if (variable.length < 3) {
-				return createOtherSetter(gl, ...variable);
-			}
-
-			const type = variable[2];
-			const subtype = variable[3];
-			const create = !subtype || type === 'sampler2D' ? createUniformSetter : createAttributeSetter;
-			return create(gl, program, ...variable);
-		});
-	});
-
-	return (gl, duration) => {
-		for (const [i, setter] of setters.entries()) {
-			let value = values[i];
-
-			if (typeof value === 'function') {
-				value = value(duration);
-			}
-
-			setter(value);
-		}
-	};
-}
-
-function createProgram (gl, programChain, siblingMap) {
-	const varyings = getStored(varyingMap, vertexStack[0], () => []);
-	const vertexShader = createShader(gl, vertexStack, varyings);
-	const fragmentShader = createShader(gl, fragmentStack, varyings);
-	const scopedVertexMap = getStored(programMap, vertexShader, () => new WeakMap());
-	
-	const program = getStored(scopedVertexMap, fragmentShader, () => {
-		const program = gl.createProgram();
-		gl.attachShader(program, vertexShader);
-		gl.attachShader(program, fragmentShader);
-		gl.linkProgram(program);
-
-		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-			console.error(gl.getProgramInfoLog(program));
-		}
-
-		return program;
-	});
-
-	const existingProgramChain = getStored(siblingMap, program, () => {
-		const chain = [program];
-		sceneChain.push(chain);
-		return chain;
-	});
-
-	const entries = [...vertexStack.slice(0, -1), ...fragmentStack.slice(0, -1)];
-	const variables = [];
-	const values = [];
-
-	for (const entry of entries) {
-		const [, entryInfo, entryValues] = entry;
-		variables.push(...entryInfo.slice(2));
-		values.push(...entryValues);
+	if (!vertexStack) {
+		return stack;
 	}
 
-	if (variables.length) {
-		const setter = createSetter(gl, program, variables, values);
-		existingProgramChain.push(setter);
+	for (const resolver of resolvers) {
+		if (typeof resolver === 'function') {
+			callbacks.push(resolver);
+			continue;
+		} else if (!Array.isArray(resolver)) {
+			continue;
+		} else if (callbacks.length) {
+			chain.push([null, ...callbacks]);
+			siblingMap.clear();
+			callbacks = [];
+		}
+
+		for (const callback of resolver) {
+			chain.push(...execute(callback, canvas, vertexStack, stack, siblingMap));
+		}
 	}
 
-	existingProgramChain.push(...programChain.slice(1));
+	if (!chain.length) {
+		const entry = createProgram(canvas, vertexStack, stack, siblingMap, ...callbacks);
+
+		if (entry) {
+			chain.push(entry)
+		}
+	} else if (callbacks.length) {
+		chain.push([null, ...callbacks]);
+	}
+
+	return chain;
 }
 
-export function extract (strings, ...values) {
-	const sequence = getStored(sequenceMap, strings, () => parse(strings));
-	const [vertexInfo, ...fragmentInfos] = sequence;
-
-	const vertexEntry = getStored(vertexStack[0][0], vertexInfo, () => {
-		return [new WeakMap(), vertexInfo];
-	});
-
-	const vertexValues = values.splice(0, vertexInfo.length - 2);
-	vertexEntry.splice(2, vertexEntry.length, vertexValues);
-
-	const fragmentEntries = fragmentInfos.map(fragmentInfo => {
-		const fragmentEntry = getStored(fragmentStack[0][0], fragmentInfo, () => {
-			return [new WeakMap(), fragmentInfo];
-		});
-
-		const resolvers = values.splice(0, fragmentInfo[0].length);
-		const fragmentValues = values.splice(0, fragmentInfo.length - 2);
-		fragmentEntry.splice(2, fragmentEntry.length, fragmentValues);
-		let programChain = [null];
-
-		for (const resolver of resolvers) {
-			if (!Array.isArray(resolver)) {
-				programChain.push(resolver);
-				continue;
-			} else if (programChain.length > 1) {
-				fragmentEntry.push(programChain);
-				programChain = [null];
-			}
-
-			fragmentEntry.push(...resolver);
-		}
-
-		if (programChain.length > 1) {
-			fragmentEntry.push(programChain);
-		}
-
-		return fragmentEntry;
-	});
-
-	return [vertexEntry, ...fragmentEntries];
-}
-
-function Program ({ gl, chain }) {
+function Program ({ canvas, chain }) {
+	const gl = getStored(rootMap, canvas, getGl);
 	const chainReference = processMemo(() => [], []);
 	chainReference.splice(0, chainReference.length, ...chain);
 
@@ -398,46 +405,17 @@ export default function compile (strings, ...values) {
 		return;
 	}
 
-	return canvas => {
-		const gl = getStored(rootMap, canvas, () => canvas.getContext('webgl2', {
-			premultipliedAlpha: /^(transparent)?$/.test(canvas.style.background),
-			antialias: !/^(crisp-edges|pixelated)$/.test(canvas.style.imageRendering),
-		}));
+	const [vertexInfo, ...fragmentInfos] = getStored(rootMap, strings, parse);
 
-		if (vertexStack.length < 2) {
-			siblingMap.clear();
+	return (canvas, vertexStack = vertexRoot, fragmentStack = fragmentRoot, siblingMap = new Map()) => {
+		const chain = [];
+		values = [...values];
+		vertexStack = extract(canvas, vertexStack, vertexInfo, values);
+
+		for (const fragmentInfo of fragmentInfos) {
+			chain.push(...extract(canvas, fragmentStack, fragmentInfo, values, vertexStack, siblingMap));
 		}
 
-		// TODO: see if this can be done more efficiently
-		// - maybe info can store a setup function that values are passed to which will extract resolvers and values for setters
-		const [vertexEntry, ...fragmentEntries] = extract(strings, ...values);
-		vertexStack.unshift(vertexEntry);
-
-		for (const fragmentEntry of fragmentEntries) {
-			fragmentStack.unshift(fragmentEntry);
-			const children = fragmentEntry.slice(3);
-
-			for (const child of children) {
-				if (typeof child === 'function') {
-					child(canvas);
-				} else if (!Array.isArray(child)) {
-					continue;
-				} else if (children.length > 1) {
-					sceneChain.push(child);
-					siblingMap.clear();
-				} else {
-					createProgram(gl, child, siblingMap);
-				}
-			}
-
-			fragmentStack.shift();
-		}
-
-		vertexStack.shift();
-
-		if (vertexStack.length < 2) {
-			const chain = sceneChain.splice(0);
-			return [Program, { gl, chain }];
-		}
+		return vertexStack.length > 2 ? chain : [Program, { canvas, chain }];
 	};
 }
