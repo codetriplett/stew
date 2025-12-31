c5cae3ec826094669a48801f279e86f686a9d348
jeffreytriplett <jeffreytriplett@users.noreply.github.com>
2025-12-30 22:45:30 -0600

    program tests

--- src/program.test.js
+++ src/program.test.js
@@ -77,6 +77,86 @@ const gl = mock({
 	'ELEMENT_ARRAY_BUFFER',
 ]);
 
+const draw = () => {};
+const before = () => {};
+const between = () => {};
+const after = () => {};
+
+function alpha (vertex, color) {
+	return compile`
+		vec3 uAlphaVertex ${vertex}
+		vec3 uVertex = uAlphaVertex;
+		${draw}
+		vec3 uAlphaColor ${color}
+		vec3 uColor = uAlphaColor;
+	`;
+}
+
+function beta (vertex, color) {
+	return compile`
+		vec3 uBetaVertex ${vertex}
+		vec3 uVertex = uBetaVertex;
+		${draw}
+		vec3 uBetaColor ${color}
+		vec3 uColor = uBetaColor;
+	`;
+}
+
+const expectedProgram = {
+	VERTEX_SHADER:
+`#version 300 es
+uniform vec3 uVertex;
+void main() {
+    gl_Position = vec4(uVertex, 0);
+}`,
+	FRAGMENT_SHADER:
+`#version 300 es
+precision mediump float;
+out vec4 gl2_FragColor;
+uniform vec3 uColor;
+void main() {
+    gl2_FragColor = vec4(uColor, 1);
+}`,
+};
+
+const expectedAlphaProgram = {
+	VERTEX_SHADER:
+`#version 300 es
+uniform vec3 uAlphaVertex;
+void main() {
+    vec3 uVertex = uAlphaVertex;
+    gl_Position = vec4(uVertex, 0);
+}`,
+	FRAGMENT_SHADER:
+`#version 300 es
+precision mediump float;
+out vec4 gl2_FragColor;
+uniform vec3 uAlphaColor;
+void main() {
+    vec3 uColor = uAlphaColor;
+    gl2_FragColor = vec4(uColor, 1);
+}`,
+};
+
+const expectedBetaProgram = {
+	VERTEX_SHADER:
+`#version 300 es
+uniform vec3 uBetaVertex;
+void main() {
+    vec3 uVertex = uBetaVertex;
+    gl_Position = vec4(uVertex, 0);
+}`,
+	FRAGMENT_SHADER:
+`#version 300 es
+precision mediump float;
+out vec4 gl2_FragColor;
+uniform vec3 uBetaColor;
+void main() {
+    vec3 uColor = uBetaColor;
+    gl2_FragColor = vec4(uColor, 1);
+}`,
+};
+
 beforeEach(() => {
 	jest.clearAllMocks();
 	canvas.getContext.mockReturnValue(gl);
@@ -86,6 +166,8 @@ beforeEach(() => {
 	gl.attachShader.mockImplementation((program, shader) => program[shader.type] = shader.code);
 	gl.getShaderParameter.mockReturnValue(true);
 	gl.getProgramParameter.mockReturnValue(true);
+	gl.getAttribLocation.mockImplementation((_, name) => name);
+	gl.getUniformLocation.mockImplementation((_, name) => name);
 	globalThis.window = {};
 });
 
@@ -198,7 +280,7 @@ describe('extract', () => {
 			vec3 uVertex ${[0, 0, 0]}
 			gl_Position = vec4(uVertex, 0);
 			${() => {}}
-			type color ${[1, 1, 1]}
+			vec3 uColor ${[1, 1, 1]}
 			gl_FragColor = vec4(uColor, 1);
 		`;
 
@@ -213,7 +295,7 @@ describe('extract', () => {
 			vec3 uVertex ${[0, 0, 0]}
 			gl_Position = vec4(uVertex, 0);
 			${() => {}}
-			type color ${[1, 1, 1]}
+			vec3 uColor ${[1, 1, 1]}
 			gl_FragColor = vec4(uColor, 1);
 		`;
 
@@ -231,70 +313,202 @@ describe('extract', () => {
 			[expect.any(Object), expect.any(Function), callback],
 		]);
 	});
-
-	// it('creates intermediate entry', () => {
-	// 	const actual = extract`
-	// 		type vertex ${123}
-	// 		position;
-	// 		${['lmno']}
-	// 		type color ${789}
-	// 		fragColor;
-	// 	`;
-
-	// 	expect(actual).toEqual([
-	// 		[new WeakMap(), expect.any(Array), [123]],
-	// 		[new WeakMap(), expect.any(Array), [789], 'lmno'],
-	// 	]);
-	// });
-
-	// it('includes surrounding resolvers', () => {
-	// 	const actual = extract`
-	// 		type vertex ${123}
-	// 		position;
-	// 		${'abc'}
-	// 		${['lmno']}
-	// 		${'xyz'}
-	// 		type color ${789}
-	// 		fragColor;
-	// 	`;
-
-	// 	expect(actual).toEqual([
-	// 		[new WeakMap(), expect.any(Array), [123]],
-	// 		[new WeakMap(), expect.any(Array), [789], [null, 'abc'], 'lmno', [null, 'xyz']],
-	// 	]);
-	// });
-
-	// it('includes multiple programs', () => {
-	// 	const actual = extract`
-	// 		type vertex ${123}
-	// 		position;
-	// 		${'abc'}
-	// 		type color ${456}
-	// 		fragColor;
-	// 		${'xyz'}
-	// 		type color ${789}
-	// 		fragColor;
-	// 	`;
-
-	// 	expect(actual).toEqual([
-	// 		[new WeakMap(), expect.any(Array), [123]],
-	// 		[new WeakMap(), expect.any(Array), [456], [null, 'abc']],
-	// 		[new WeakMap(), expect.any(Array), [789], [null, 'xyz']],
-	// 	]);
-	// });
 });
 
-describe.skip('compile', () => {
-	it('creates final entry', () => {
-		const actual = compile`
-			type vertex ${123}
-			position;
-			${() => {}}
-			type color ${789}
-			fragColor;
+describe('compile', () => {
+	it('simple program', () => {
+		const render = compile`
+			vec3 uVertex ${[0, 0, 0]}
+			gl_Position = vec4(uVertex, 0);
+			${draw}
+			vec3 uColor ${[1, 1, 1]}
+			gl_FragColor = vec4(uColor, 1);
 		`;
 
-		const layout = actual(canvas);
-		layout[0]();
+		const { chain } = render(canvas)[1];
+		
+		expect(chain).toEqual([
+			[expectedProgram, expect.any(Function), draw],
+		]);
+
+		chain[0][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uVertex', [0, 0, 0]],
+			['uColor', [1, 1, 1]],
+		]);
+	});
+	
+	it('nested program', () => {
+		const render = compile`
+			gl_Position = vec4(uVertex, 0);
+			${[compile`
+				vec3 uVertex ${[0, 0, 0]}
+				${draw}
+				vec3 uColor ${[1, 1, 1]}
+			`]}
+			gl_FragColor = vec4(uColor, 1);
+		`;
+
+		const { chain } = render(canvas)[1];
+		
+		expect(chain).toEqual([
+			[expectedProgram, expect.any(Function), draw],
+		]);
+
+		chain[0][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uVertex', [0, 0, 0]],
+			['uColor', [1, 1, 1]],
+		]);
+	});
+	
+	it('multiple programs', () => {
+		const render = compile`
+			gl_Position = vec4(uVertex, 0);
+			${[alpha([0, 0, 0], [1, 1, 1]), alpha([2, 2, 2], [3, 3, 3])]}
+			${[beta([4, 4, 4], [5, 5, 5]), beta([6, 6, 6], [7, 7, 7])]}
+			gl_FragColor = vec4(uColor, 1);
+		`;
+
+		const { chain } = render(canvas)[1];
+		
+		expect(chain).toEqual([
+			[expectedAlphaProgram, expect.any(Function), draw, expect.any(Function), draw],
+			[expectedBetaProgram, expect.any(Function), draw, expect.any(Function), draw],
+		]);
+
+		chain[0][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uAlphaVertex', [0, 0, 0]],
+			['uAlphaColor', [1, 1, 1]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[0][3]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uAlphaVertex', [2, 2, 2]],
+			['uAlphaColor', [3, 3, 3]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[1][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uBetaVertex', [4, 4, 4]],
+			['uBetaColor', [5, 5, 5]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[1][3]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uBetaVertex', [6, 6, 6]],
+			['uBetaColor', [7, 7, 7]],
+		]);
+	});
+	
+	it('sorts programs', () => {
+		const render = compile`
+			gl_Position = vec4(uVertex, 0);
+			${[alpha([0, 0, 0], [1, 1, 1]), beta([4, 4, 4], [5, 5, 5]), alpha([2, 2, 2], [3, 3, 3]), beta([6, 6, 6], [7, 7, 7])]}
+			gl_FragColor = vec4(uColor, 1);
+		`;
+
+		const { chain } = render(canvas)[1];
+		
+		expect(chain).toEqual([
+			[expectedAlphaProgram, expect.any(Function), draw, expect.any(Function), draw],
+			[expectedBetaProgram, expect.any(Function), draw, expect.any(Function), draw],
+		]);
+
+		chain[0][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uAlphaVertex', [0, 0, 0]],
+			['uAlphaColor', [1, 1, 1]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[0][3]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uAlphaVertex', [2, 2, 2]],
+			['uAlphaColor', [3, 3, 3]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[1][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uBetaVertex', [4, 4, 4]],
+			['uBetaColor', [5, 5, 5]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[1][3]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uBetaVertex', [6, 6, 6]],
+			['uBetaColor', [7, 7, 7]],
+		]);
+	});
+	
+	it('surrounding resolvers', () => {
+		const render = compile`
+			gl_Position = vec4(uVertex, 0);
+			${before}
+			${[alpha([0, 0, 0], [1, 1, 1]), beta([4, 4, 4], [5, 5, 5])]}
+			${between}
+			${[alpha([2, 2, 2], [3, 3, 3]), beta([6, 6, 6], [7, 7, 7])]}
+			${after}
+			gl_FragColor = vec4(uColor, 1);
+		`;
+
+		const { chain } = render(canvas)[1];
+		
+		expect(chain).toEqual([
+			[null, before],
+			[expectedAlphaProgram, expect.any(Function), draw],
+			[expectedBetaProgram, expect.any(Function), draw],
+			[null, between],
+			[expectedAlphaProgram, expect.any(Function), draw],
+			[expectedBetaProgram, expect.any(Function), draw],
+			[null, after],
+		]);
+
+		chain[1][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uAlphaVertex', [0, 0, 0]],
+			['uAlphaColor', [1, 1, 1]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[4][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uAlphaVertex', [2, 2, 2]],
+			['uAlphaColor', [3, 3, 3]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[2][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uBetaVertex', [4, 4, 4]],
+			['uBetaColor', [5, 5, 5]],
+		]);
+
+		gl.uniform3fv.mockClear();
+		chain[5][1]();
+
+		expect(gl.uniform3fv.mock.calls).toEqual([
+			['uBetaVertex', [6, 6, 6]],
+			['uBetaColor', [7, 7, 7]],
+		]);
 	});
 });
